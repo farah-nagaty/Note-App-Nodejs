faa3aa6cecbfeec1d12262723c51a2ce0a1a7ed8
Farah Nagaty <fara7nagaty@gmail.com>
2026-03-30 19:04:47 +0200

    Note App First Step

---
+++ app.js
@@ -0,0 +1,27 @@
+// Dot Env
+require ("dotenv").config();
+// Express
+const express = require ("express");
+const app = express();
+// Middleware
+app.use(express.json());
+// Port
+const port = process.env.PORT || 5000;
+// Datbase Connection
+ const mongoose = require("mongoose");
+
+async function dbconnection() {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Connected!");
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+dbconnection();
+// Run Server "Listen"
+
+app.listen(port, ()=>{
+    console.log(`Server is running at port ${port}`);
+});
